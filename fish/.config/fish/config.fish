924744f249f028c23869cc2f54b0e40b3cbbe259
Henrique Shen <hshen2908@gmail.com>
2023-07-10 02:01:29 -0300

    Fixed display refresh rate

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-	pokemon-colorscripts -r --no-title &
+	pokemon-colorscripts -r --no-title
 	starship init fish | source &
 	#thefuck --alias | source &
 	~/.config/fish/tty.sh &
